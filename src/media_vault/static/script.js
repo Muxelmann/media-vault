156a5b6aaf9863256393f678765a45d983502e57
Max Zangs <maximilian.zangs@gmail.com>
2024-02-10 19:22:25 +0100

    Enable arrow based navigation using keyboard

--- src/media_vault/static/script.js
+++ src/media_vault/static/script.js
@@ -7,5 +7,23 @@ function toggleFavorite(element) {
 
 function toggleView() {
     let itemList = document.getElementById('item-list');
-    itemList.classList.toggle("column");
+    itemList.classList.toggle('column');
 }
+
+document.addEventListener('keyup', (e) => {
+    if (e.key == 'ArrowLeft') {
+        console.log('back');
+        let a = document.getElementById('previous-neighbor');
+        if (a) {
+            let href = a.getAttribute('href');
+            window.open(href, '_self');
+        }
+    } else if (e.key == 'ArrowRight') {
+        console.log('next');
+        let a = document.getElementById('next-neighbor');
+        if (a) {
+            let href = a.getAttribute('href');
+            window.open(href, '_self');
+        }
+    }
+});
