4234a98281d3d76db8589bd7a0c3d4b3ca8b60d8
Max Zangs <maximilian.zangs@gmail.com>
2024-02-20 20:28:58 +0100

    separating functionality

--- src/media_vault/database/__init__.py
+++ src/media_vault/database/__init__.py
@@ -15,15 +15,22 @@ class Database:
             raise Exception('Database Location not initialized')
 
         self.connection = sqlite3.connect(Database.DB_LOCATION)
+        self.connection.execute('PRAGMA foreign_keys = ON')
         self.cursor = self.connection.cursor()
 
     def __del__(self):
-        self.connection.commit()
+        self.commit()
         self.cursor.close()
         self.connection.close()
 
     def execute(self, sql: str, vars: tuple[str] = (), fetchone: bool = False, fetchmany: int = 0):
         self.cursor.execute(sql, vars)
+        return self.fetch(fetchone, fetchmany)
+
+    def commit(self):
+        self.connection.commit()
+
+    def fetch(self, fetchone: bool = False, fetchmany: int = 0):
         if fetchone:
             return self.cursor.fetchone()
         if fetchmany > 0:
